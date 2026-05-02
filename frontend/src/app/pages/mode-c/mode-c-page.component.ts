640b7cca0bde971c4fc9edd10dc434c7c92501d2
aaaassswwww <23302010020@m.fudan.edu.cn>
2026-05-02 10:33:09 +0800

    将cnn-explainer嵌入到了mode-c的一个容器内，因为cnn-explainer本身是用的svelte实现，所以这种方法比较方便，功能也全部保留了，但是模块c的登录态还没有共享

--- frontend/src/app/pages/mode-c/mode-c-page.component.ts
+++ frontend/src/app/pages/mode-c/mode-c-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,4 +8,13 @@ import { RouterLink } from '@angular/router';
   templateUrl: './mode-c-page.component.html',
   styleUrl: './mode-c-page.component.css'
 })
-export class ModeCPageComponent {}
+export class ModeCPageComponent {
+  readonly moduleUrl: SafeResourceUrl;
+  readonly assetTarget = '/modules/cnn-explainer/index.html';
+  readonly sourceProjectPath = 'D:\\VS Code\\cnn-explainer';
+  readonly syncCommand = '.\\scripts\\sync-cnn-explainer.ps1';
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.moduleUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.assetTarget);
+  }
+}
